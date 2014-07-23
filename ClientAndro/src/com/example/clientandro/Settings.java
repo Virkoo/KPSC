5d0f8ec6aceb2294e8e4d8ab9544ea7a48633e63
John <vircdusan@gamil.com>
2014-07-23 12:38:11 +0200

    dodate scener klase

--- ClientAndro/src/com/example/clientandro/Settings.java
+++ ClientAndro/src/com/example/clientandro/Settings.java
@@ -1,23 +1,15 @@
 package com.example.clientandro;
 
-import java.io.IOException;
-
 import transfer.Klijent;
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
-import android.text.Editable;
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-import android.os.Build;
 
 public class Settings extends Activity {
 	
